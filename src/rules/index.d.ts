5cbdccc9661e700221b94ecf9d1c8b428eb83e2c
HavocZhang <zhanghaojie@ai-wsd.com>
2023-10-10 09:38:48 +0800

    chore: 抄antfu大佬

--- src/rules/index.d.ts
+++ src/rules/index.d.ts
@@ -1,3 +1,4 @@
+import { MergeIntersection } from '../utils';
 import type { DeprecationRules } from './deprecation';
 import type { EslintRules } from './eslint';
 import type { EslintCommentsRules } from './eslint-comments';
@@ -23,12 +24,10 @@ import type { VueI18nRules } from './vue-i18n';
 import type { VuePugRules } from './vue-pug';
 import type { YmlRules } from './yml';
 
-/**
- * Rules.
- *
- * @see [Rules](https://eslint.org/docs/user-guide/configuring/rules)
- */
-export type Rules = Partial<
+export * from './rule-config';
+export * from './rule-severity';
+
+export type BuiltinRules = MergeIntersection<
   DeprecationRules &
     EslintRules &
     EslintCommentsRules &
@@ -51,6 +50,12 @@ export type Rules = Partial<
     VueRules &
     VueI18nRules &
     VuePugRules &
-    YmlRules &
-    Record<string, RuleConfig>
+    YmlRules
 >;
+
+/**
+ * Rules.
+ *
+ * @see [Rules](https://eslint.org/docs/user-guide/configuring/rules)
+ */
+export type Rules = Partial<BuiltinRules & Record<string, RuleConfig>>;
